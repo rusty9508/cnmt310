8fd6cdab3c95dd8016103ecb8a33a4c5b5c907db
rusty9508 <44064674+rusty9508@users.noreply.github.com>
2018-10-11 17:47:28 -0500

    Add files via upload

---
+++ bookQuery.sql
@@ -0,0 +1,11 @@
+SELECT * FROM melville_a.bookinfo;
+
+Insert into bookinfo values(1, NULL, '1984', 1, 'George Orwell', 'OK');
+
+Insert into bookinfo values(2, NULL, 'Moby Dick', 1, 'Herman Melville', 'OK');
+
+Insert into bookinfo values(3, NULL, 'Titanic', 1, 'Filson Young', 'OK');
+
+Insert into bookinfo values(4, NULL, 'Catch 22', 1, 'Joseph Heller', 'OK');
+
+Insert into bookinfo values(5, NULL, 'Harry Potter', 1, 'J. K. Rowling', 'OK');
