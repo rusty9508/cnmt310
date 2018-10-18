19419f90be65089bcd9f1f3e4c82ac9156c94256
rusty9508 <44064674+rusty9508@users.noreply.github.com>
2018-10-18 17:53:10 -0500

    Add files via upload

---
+++ createTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contactSubmit (
+id int not null primary key auto_increment,
+firstname varchar(255),
+lastname varchar(255),
+email varchar(255),
+phonenumber varchar(255),
+comments varchar(255)
+);
