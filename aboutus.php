b370852b912467b95d6486ba92dc8a6b7a4ea2cc
rusty9508 <44064674+rusty9508@users.noreply.github.com>
2018-10-23 11:01:14 -0500

    Add files via upload
    Revisions to aboutus page

--- aboutus.php
+++ aboutus.php
@@ -40,13 +40,13 @@ print "<p>At Huawei, innovation focuses on customer needs. We invest heavily in 
 print "</div> \n";
 print "<div class='video'> \n";
 print "<iframe width='550' height='350' \n"; 
-print "src='https://www.youtube.com/embed/9g6kw1fu1Io'  allow='autoplay; encrypted-media' allowfullscreen></iframe> \n";
+print "src='https://www.youtube.com/embed/9gFFuXJIPVE'  allow='autoplay; encrypted-media' allowfullscreen></iframe> \n";
 print "</div> \n";
 print "</div> \n";
 print "<div class='border2'> \n";	
-print "<img src='http://usa.chinadaily.com.cn/business/attachement/jpg/site1/20130929/180373d2873013b2a13a34.jpg' alt='1picture' width='550' height='400'/> \n";	
-print "<img src='https://www.cmimagazine.it/wp-content/uploads/2017/12/Huawei-experience-store.jpg' alt='2picture' width='550' height='400'/> \n";
-print "<img src='https://cdn.techinasia.com/wp-content/uploads/2012/10/huawei-ceatec-7-680x435.jpg' alt='3picture' width='550' height='400'/> \n";
+print "<img src='Pictures/books1.jpg' alt='Books' width='550' height='400'/> \n";	
+print "<img src='Pictures/books2.jpg' alt='Books' width='550' height='400'/> \n";
+print "<img src='Pictures/books3.jpg' alt='And more books' width='550' height='400'/> \n";
 print "</div> \n";
 
 /*for nav and bottom*/print $myPage->getBottomSection();
